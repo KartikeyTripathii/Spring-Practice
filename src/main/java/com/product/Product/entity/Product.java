3addfeba7ba5467cc04c8a7ec940832a0fb715b0
KartikeyTripathii <kuruhere1234@gmail.com>
2025-12-03 10:42:12 +0530

    sligh change

--- src/main/java/com/product/Product/entity/Product.java
+++ src/main/java/com/product/Product/entity/Product.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,7 @@ public class Product {
     private Double price;
 
     @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
 }
